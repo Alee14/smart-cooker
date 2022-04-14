796dc37e648f1c8c44ffe990cedd0be17669fabc
Andrew Lee <alee14498@protonmail.com>
2022-04-14 00:59:35 -0400

    Even more content

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-       <Image src="/logov1-2.png" width={256} height={256}/>
-       <h2>L&apos;été est là!<br/>L&apos;argent, c&apos;est maintenant !</h2>
+       <Image src="/logov1-2.png" width={256} height={256} alt="Logo"/>
+       <h2 className={styles.center}>L&apos;été est là!<br/>L&apos;argent, c&apos;est maintenant!</h2>
+       <h3>Modèle prototype</h3>
+       <Image src="/smart-stove.png" width={500} height={300} alt="Render image"/>
+       <h3>Description</h3>
+       <p>Maître Cuisinier est un cuiseur portable pour les camionneurs de nourriture et il possède le premier cuiseur à écran tactile au monde.</p>
+       <h3 className={styles.center}>Achetez ce produit à 300 $ avec une remise de 10 %.<br/> Contactez admin@maitre-cuisinier.ca pour commander votre cuisinière.</h3>
       </main>
     </div>
   )
